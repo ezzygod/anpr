10a5f06774a687bbca118b7ba2c85fba97e1cb0f
ezzygod <147803799+ezzygod@users.noreply.github.com>
2025-04-05 13:54:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -39,8 +39,3 @@ async def process_image(file: UploadFile = File(...)):
     save_to_csv([[p["text"], p["confidence"]] for p in plates_detected])
     
     return {"plates": plates_detected}
-
-if __name__ == "__main__":
-    import uvicorn
-    port = int(os.environ.get("PORT", 8000))  # Render setează portul în env
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
