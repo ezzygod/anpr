d8677110425c84e6a941ca6e000404b56719f189
ezzygod <147803799+ezzygod@users.noreply.github.com>
2025-04-05 13:01:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -40,4 +40,6 @@ async def process_image(file: UploadFile = File(...)):
     return {"plates": plates_detected}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))  # Render setează portul în env
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
