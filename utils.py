bbc71732a5ae24e3ccc8e7819cceec6e4bab66f1
ezzygod <147803799+ezzygod@users.noreply.github.com>
2025-04-17 15:26:07 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -12,7 +12,7 @@ judete = {
     'CS', 'CL', 'CJ', 'CT', 'CV', 'DB', 'DJ', 'GL', 'GR', 'GJ',
     'HR', 'HD', 'IL', 'IS', 'IF', 'MM', 'MH', 'MS', 'NT', 'OT',
     'PH', 'SM', 'SJ', 'SB', 'SV', 'TR', 'TM', 'TL', 'VS', 'VL',
-    'VN', 'BR', 'B'  # B e București
+    'VN', 'BR', 'B'
 }
 
 def generate_variants(text):
